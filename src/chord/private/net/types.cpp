85a6f634d890771de656b3b19efa52d8f21dcd19
sneppy <andrea.mecchia@mail.polimi.it>
2019-05-05 19:55:54 +0200

    Added network types and functions

---
+++ src/chord/private/net/types.cpp
@@ -0,0 +1,182 @@
+#include "net/types.h"
+
+namespace Net
+{
+	template<>
+	bool parseIpString(Ipv4 & addr, const char * str)
+	{
+		uint32 n = 0;
+		uint32 bit = 0;
+		ubyte _ = 1;
+		const char * cc = str;
+
+		// Get ip
+		for (char c = *cc; _; c = *++cc)
+		{
+			switch (c)
+			{
+				case ':':
+					// Break and read port
+					_ = 0;
+				case '.':
+					// Add component to out ip
+					addr.host |= n << bit; n = 0;
+
+					// Next byte
+					bit += 8U;
+					break;
+
+				case '\0':
+				case '\n':
+				case '\r':
+					// Add component
+					addr.host |= n << bit; n = 0;
+
+					// No port, return
+					// If we read at least one character, parsing is successful
+					return cc != str;
+			
+				default:
+					// Must be a number
+					if (c < '0' || c > '9')
+						return false;
+
+					n *= 10;
+					n += c - '0';
+					break;
+			}
+		}
+
+		// Get port
+		for (char c = *cc; c >= '0' && c <= '9'; c = *++cc)
+			n *= 10, n += *cc - '0';
+		addr.port = htons(n);
+
+		// Parsing OK
+		return true;
+	}
+
+	template<>
+	String getIpString(const Ipv4 & addr)
+	{
+		char str[INET_ADDRSTRLEN + 1 + 6];
+
+		// Ip
+		inet_ntop(AF_INET, &addr.host, str, INET_ADDRSTRLEN);
+		// Port
+		snprintf(str + 17, 6, "%u", htons(addr.port));
+
+		return String(str) + ":" + String(str + 17);
+	}
+
+	template<>
+	bool getInterfaceAddr(Ipv4 & addr)
+	{
+		// Stash port (network byte order)
+		uint16 port = addr.port;
+
+		ifaddrs * ifs;
+		if (getifaddrs(&ifs) == 0)
+		{
+			for (auto it = ifs; it != nullptr; it = it->ifa_next)
+			{
+				if (it->ifa_addr && it->ifa_addr->sa_family == AF_INET && !(it->ifa_flags & IFF_LOOPBACK))
+				{
+					addr.__addr = *it->ifa_addr;
+					addr.port = port;
+
+					freeifaddrs(ifs);
+					return true;
+				}
+			}
+			
+			// Dealloc list
+			freeifaddrs(ifs);
+		}
+
+		return false;
+	}
+
+	template<>
+	bool getHostAddr(Ipv4 & addr, const char * hostname)
+	{
+		// Stash port (network byte order)
+		uint16 port = addr.port;
+
+		// Copy hostname
+		String hn(hostname);
+
+		// Parse port (if any)
+		{
+			char * it = *hn, * const end = *hn + hn.getLength();
+			for (char c = *it; c != ':' && it != end; c = *++it);
+
+			if (it != end)
+			{
+				// Reset default port
+				port = 0;
+
+				// Terminate here, otherwise getaddrinfo doesn't work
+				*it = '\0';
+
+				// Parse port
+				for (char c = *++it; it != end; c = *++it)
+					if (c >= '0' && c <= '9') port *= 10, port += c - '0';
+				port = htons(port);
+			}
+		}
+
+		addrinfo * ais;
+		addrinfo * hint = new addrinfo; hint->ai_family = AF_INET;
+		if (getaddrinfo(*hn, nullptr, hint, &ais) == 0)
+		{
+			for (auto it = ais; it != nullptr; it = it->ai_next)
+			{
+				if (it->ai_addr)
+				{
+					addr.__addr = *it->ai_addr;
+					addr.port = port;
+
+					delete hint;
+					freeaddrinfo(ais);
+					return true;
+				}
+			}
+
+			// Free list
+			freeaddrinfo(ais);
+		}
+
+		delete hint;
+		return false;
+	}
+
+	template<>
+	LinkedList<Ipv4> getHostAddrs(const char * hostname, uint16 defPort)
+	{
+		// TODO: handle port
+
+		LinkedList<Ipv4> out;
+		addrinfo * ais;
+		addrinfo * hint = new addrinfo; hint->ai_family = AF_INET;
+		if (getaddrinfo(hostname, nullptr, hint, &ais) == 0)
+		{
+			for (auto it = ais; it != nullptr; it = it->ai_next)
+			{
+				if (it->ai_addr)
+				{
+					Ipv4 addr = Ipv4{__addr : *it->ai_addr};
+					addr.setPort(defPort);
+
+					out.push(addr);
+				}
+			}
+
+			// Free list
+			freeaddrinfo(ais);
+		}
+
+		delete hint;
+		return out;
+	}
+} // namespace Net
---
+++ src/chord/public/net/types.h
@@ -0,0 +1,140 @@
+#pragma once
+
+#include "coremin.h"
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <net/if.h>
+#include <ifaddrs.h>
+#include <netdb.h>
+
+namespace Net
+{
+	union Ipv4
+	{
+		struct
+		{
+			/// Address family (ipv4 or ipv6)
+			uint16 family = AF_INET;
+
+			/// Address port
+			uint16 port;
+
+			/// Ipv4
+			union
+			{
+				uint32 host;
+				ubyte hostBytes[4];
+			};
+		};
+
+		// Legacy structure
+		sockaddr __addr;
+
+	public:
+		/// Get port (host byte order)
+		FORCE_INLINE uint16 getPort() const
+		{
+			return ntohs(port);
+		}
+
+		/// Set port (host byte order)
+		FORCE_INLINE void setPort(uint16 _port)
+		{
+			port = htons(_port);
+		}
+	};
+
+	union Ipv6
+	{
+		struct
+		{
+			/// Address family
+			uint16 family = AF_INET6;
+
+			/// Address port
+			uint16 port;
+
+			/// Ipv6 network prefix
+			union
+			{
+				uint64 network;
+				uint16 networkBytes[4];
+			};
+
+			/// Ipv6 host
+			union
+			{
+				uint64 host;
+				uint16 hostBytes[4];
+			};
+		};
+
+		/// Legacy structure
+		sockaddr __addr;
+
+	public:
+		/// Get port (host byte order)
+		FORCE_INLINE uint16 getPort() const
+		{
+			return ntohs(port);
+		}
+
+		/// Set port (host byte order)
+		FORCE_INLINE void setPort(uint16 _port)
+		{
+			port = htons(_port);
+		}
+	};
+
+	/**
+	 * Get ip address from string
+	 * 
+	 * @param [in] str address string
+	 * @param [in] defPort default port if none specified (in host byte order)
+	 * @return true if correctly parsed
+	 */
+	template<typename IpType>
+	bool parseIpString(IpType & addr, const char * str);
+
+	/**
+	 * Get string from ip
+	 * 
+	 * @param [in] addr ip address
+	 * @return ip string
+	 */
+	template<typename IpType = Ipv4>
+	String getIpString(const IpType & addr);
+
+	/**
+	 * Get address of first available interface
+	 * 
+	 * @param [in] family interface type (AF_INET, AF_INET6)
+	 * @param [in] port optional port (host byte order)
+	 * @return requested address
+	 * @{
+	 */
+	template<typename IpType = Ipv4>
+	bool getInterfaceAddr(IpType & addr);
+
+	template<typename IpType = Ipv4>
+	LinkedList<IpType> getInterfaceAddrs(uint16 port = 0);
+	/// @}
+
+
+	/**
+	 * Returns host ip address(es)
+	 * 
+	 * @param [in] hostname hostname to resolve
+	 * @param [in] port port (host byte order)
+	 * @return first address or list of address
+	 * @{
+	 */
+	template<typename IpType = Ipv4>
+	bool getHostAddr(IpType & addr, const char * hostname);
+
+	template<typename IpType = Ipv4>
+	LinkedList<IpType> getHostAddrs(const char * hostname, uint16 defPort = 0);
+	/// @}
+} // namespace Net
